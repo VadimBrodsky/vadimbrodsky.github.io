1eb67594f982708d833ba34f4987aacb7e70a043
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-06-18 23:01:19 -0400

    Cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,6 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
 
 import useSiteMetadata from '../hooks/use-site-metadata';
 import Header from './header';
