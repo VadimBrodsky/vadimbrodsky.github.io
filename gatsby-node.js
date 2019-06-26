6e17a88c19817d733a6440c95c681ed5aa3e0692
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-06-25 23:58:57 -0400

    WIP generate posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,34 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panic('Failed to create posts', result.errors);
+  }
+
+  const posts = result.data.allMdx.nodes;
+
+  posts.forEach((post) => {
+    createPage({
+      path: `/posts${node.frontmatter.slug}`,
+      component: require.resolve('./src/templates/post.js'),
+      context: {
+        slug: post.frontmatter.slug,
+      },
+    });
+  });
+};
---
+++ src/templates/post.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from './layout';
+
+const PostTemplate = () => {
+  return (
+    <Layout>
+      <h1>Post Layout</h1>
+    </Layout>
+  );
+};
+
+export default PostLayout;
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
+  }
+`;
