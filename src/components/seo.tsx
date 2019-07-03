40acbdc5cbf5946dc6deee32c56244dbf36a704e
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-07-03 07:16:32 -0400

    Rework helmet and start adding global styles

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import * as React from 'react';
 import Helmet from 'react-helmet';
 
@@ -13,6 +6,41 @@ import useSiteMetadata from '../hooks/use-site-metadata';
 function SEO({ description, lang, meta, title }) {
   const { description: metaDescription = description, author } = useSiteMetadata();
 
+  const defaultMeta = [
+    {
+      name: `description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: author,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: metaDescription,
+    },
+  ];
+
   return (
     <Helmet
       htmlAttributes={{
@@ -20,40 +48,7 @@ function SEO({ description, lang, meta, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
+      meta={[...defaultMeta, ...meta]}
     />
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import Helmet from 'react-helmet';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
@@ -7,6 +8,11 @@ import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <html lang="en" className="text-gray-900 antialiased leading-tight" />
+      <body className="bg-gray-100" />
+    </Helmet>
+
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
